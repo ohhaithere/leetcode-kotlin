108d8fb6ed2306d74b32510ac2cbc931c618281c
ohhaithere <frostmasterwork@gmail.com>
2022-04-28 20:53:31 +0300

    Counting Bits

---
+++ src/com/mrazi/leetcode/CountingBits.kt
@@ -0,0 +1,11 @@
+package com.mrazi.leetcode
+
+fun countBits(n: Int): IntArray {
+    val ans = IntArray(n + 1)
+
+    for (i in 0..n) {
+        ans[i] = Integer.bitCount(i)
+    }
+
+    return ans
+}
