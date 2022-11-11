fce0f259e43b0c4012242c743cd67040d13fc710
ohhaithere <frostmasterwork@gmail.com>
2022-11-11 20:17:16 +0300

    godlike refactoring

--- src/com/mrazi/leetcode/FindPivotIndex.kt
+++ src/com/mrazi/leetcode/FindPivotIndex.kt
@@ -4,7 +4,7 @@ fun pivotIndex(nums: IntArray): Int {
     var sum = 0
     var leftsum = 0
     for (x in nums) sum += x
-    for (i in 0 until nums.size) {
+    for (i in nums.indices) {
         if (leftsum == sum - leftsum - nums[i]) return i
         leftsum += nums[i]
     }
